0cc4ee7c4ca0020852636681e6ec366dfd7147dd
kissetfall <bogpill@ya.ru>
2021-10-17 19:06:45 +0300

    upd

--- keyboards/sofle/keymaps/ehrgb/config.h
+++ keyboards/sofle/keymaps/ehrgb/config.h
@@ -5,9 +5,9 @@
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
+define MASTER_LEFT
 //#define MASTER_RIGHT
-//#define EE_HANDS
+#define EE_HANDS
 //#define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
