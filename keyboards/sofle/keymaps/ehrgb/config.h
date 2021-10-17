4193d1059b5e580ec8ea4a12d8138a90474073d3
kissetfall <bogpill@ya.ru>
2021-10-17 18:56:48 +0300

    upd

--- keyboards/sofle/keymaps/ehrgb/config.h
+++ keyboards/sofle/keymaps/ehrgb/config.h
@@ -5,9 +5,9 @@
 
 /* Select hand configuration */
 
-//#define MASTER_LEFT
+#define MASTER_LEFT
 //#define MASTER_RIGHT
-#define EE_HANDS
+//#define EE_HANDS
 //#define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
@@ -23,17 +23,16 @@
 #define RGBLED_NUM 72  // Number of LEDs
 #define RGBLED_SPLIT { 36, 36 } // haven't figured out how to use this yet 
 //#define DRIVER_LED_TOTAL 72
-//#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_LIMIT_VAL 150
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
 
-#define RGBLIGHT_EFFECT_STATIC_LIGHT
+//#define RGBLIGHT_EFFECT_STATIC_LIGHT
 //#define RGBLIGHT_EFFECT_BREATHING
 //#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL
 #endif
-
 
