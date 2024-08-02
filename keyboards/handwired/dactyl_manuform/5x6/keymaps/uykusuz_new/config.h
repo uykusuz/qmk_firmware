bbd888bee3d4850a5bcb52bdbae9409d5a172db6
Boris Brönner <broenner@xelonic.com>
2024-08-02 11:25:03 +0200

    switch to left master

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/uykusuz_new/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/uykusuz_new/config.h
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL
 
-//#define MASTER_LEFT
-#define MASTER_RIGHT
+#define MASTER_LEFT
+//#define MASTER_RIGHT
 //#define EE_HANDS
 
 #define TAPPING_TERM 200
